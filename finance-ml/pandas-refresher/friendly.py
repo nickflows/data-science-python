213cb5a4e4f5909eb549e128a7872f4c71e12c6f
Nick <nicholasj.flores@gmail.com>
2024-08-13 17:20:14 -0700

    practice module on pandas
    Practice module from this YouTube for a coursera class: https://www.youtube.com/watch?v=5rNu16O3YNE

---
+++ finance-ml/pandas-refresher/friendly.py
@@ -0,0 +1,10 @@
+# a module that I am going to use as an example
+
+
+def greet(what="Hello",to="Mystery Person"):
+    """
+    """
+    return f'{what} {to} (changed!)'
+    
+    
+    
---
+++ finance-ml/prework-finance-ml.md
@@ -0,0 +1,6 @@
+# Introduction to Portfolio Construction w/ Python 
+
+
+
+
+range(5,10)[-1]
